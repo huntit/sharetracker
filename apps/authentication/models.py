741e803fddbdae85c2ad278b540a60a6e62e0b68
Peter Hunt <huntit7@gmail.com>
2025-10-31 14:46:43 +1000

    Save work before moving repository to top level

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -9,6 +9,13 @@ from apps import db, login_manager
 
 from apps.authentication.util import hash_pass
 
+from flask import current_app as app
+
+import os, csv
+
+from sqlalchemy.orm import relationship
+
+
 class Users(db.Model, UserMixin):
 
     __tablename__ = 'Users'
@@ -17,6 +24,7 @@ class Users(db.Model, UserMixin):
     username = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(64), unique=True)
     password = db.Column(db.LargeBinary)
+    children = relationship("Portfolio")
 
     def __init__(self, **kwargs):
         for property, value in kwargs.items():
@@ -51,9 +59,56 @@ def request_loader(request):
 class Portfolio(db.Model):
     __tablename__ = 'Portfolio'
     id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.String(6), unique=True, nullable=False)
-    units = db.Column(db.Integer, unique=False)
-    last = db.Column(db.Numeric(10,3))
+    user_id = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=False)
+    name = db.Column(db.String(256), unique=False, nullable=True)
+    notes = db.Column(db.Text, unique=False, nullable=True)
+    children = relationship("Portfolio_Stock")
+
+    # constructor
+    def __init__(self, user_id, name, notes):
+        self.user_id = user_id
+        self.name = name
+        self.notes = notes
 
     def __repr__(self):
-        return f"Portfolio(code = {self.code}, units = {self.units}, last = {self.last})"
+        return f"Portfolio(user_id = {self.user_id}, name = {self.name}, notes = {self.notes})"
+
+class Portfolio_Stock(db.Model):
+    __tablename__ = 'Portfolio_Stock'
+    id = db.Column(db.Integer, primary_key=True)
+    portfolio_id = db.Column(db.Integer, db.ForeignKey('Portfolio.id'), nullable=False)
+    stock_code = db.Column(db.String(6), db.ForeignKey('Stock.code'), nullable=False)
+    units = db.Column(db.Integer, nullable=False)
+    notes = db.Column(db.Text, unique=False, nullable=True)
+    children = relationship("Stock")
+
+    def __repr__(self):
+        return f"Portfolio_Stock(portfolio_id = {self.portfolio_id}, stock_code = {self.stock_code}, units = {self.units}, notes = {self.notes})"
+
+
+class Stock(db.Model):
+    __tablename__ = 'Stock'
+    code = db.Column(db.String(6), primary_key=True)
+    short_name = db.Column(db.String(256), unique=False)
+    last = db.Column(db.Numeric(10, 3))
+
+    def __repr__(self):
+        return f"Stock(code = {self.code}, short_name = {self.short_name}, last = {self.last})"
+
+# This function runs on table creation, to pre-populate data from a static CSV file
+@db.event.listens_for(Stock.__table__, 'after_create')
+def create_stocks(*args, **kwargs):
+
+    # Pre-populate stock data by importing from a Metastock CSV file
+    # Example of CSV:
+    # ANZ, 220801, 22.8, 22.8, 22.42, 22.74, 7611230
+    filename = f'{app.root_path}/static/ASXEQUITIESMetastock.txt'
+    with open(filename, mode='r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for index, row in enumerate(csv_reader):
+          app.logger.info(row)
+          db.session.add(Stock(code=row[0], short_name='', last=row[5]))
+
+    db.session.commit()
+
+
--- apps/home/routes.py
+++ apps/home/routes.py
@@ -2,18 +2,30 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
-
 from apps.home import blueprint
-from flask import render_template, request
-from flask_login import login_required
+from flask import render_template, redirect, request, url_for
+from flask_login import login_required, current_user
 from jinja2 import TemplateNotFound
-from apps.authentication.models import Portfolio
+from apps.authentication.models import Portfolio, Portfolio_Stock, Stock
+from apps import db
+from collections import namedtuple
 
 @blueprint.route('/index')
 @login_required
 def index():
 
     return render_template('home/index.html', segment='index')
+
+@blueprint.route('/add_portfolio', methods=['POST'])
+@login_required
+def add_portfolio():
+    if request.method == 'POST':
+        name = request.form['portfolio_name']
+        notes = request.form['portfolio_notes']
+        new_portfolio = Portfolio(current_user.id, name, notes)
+        db.session.add(new_portfolio)
+        db.session.commit()
+        return redirect('/portfolio.html')
 
 
 @blueprint.route('/<template>')
@@ -29,8 +41,26 @@ def route_template(template):
         segment = get_segment(request)
 
         if segment == 'portfolio.html':
-            portfolio = Portfolio.query.all()
-            return render_template("home/" + template, segment=segment, portfolio=portfolio)
+            # https://stackoverflow.com/questions/17972020/how-to-execute-raw-sql-in-flask-sqlalchemy-app
+            # SELECT * FROM Portfolio INNER JOIN Portfolio_Stock ON Portfolio.id == Portfolio_Stock.portfolio_id
+            # INNER JOIN Stock ON Portfolio_Stock.stock_code == Stock.code WHERE Portfolio.user_id == 1
+            result = db.session.execute("""SELECT Portfolio.id, Portfolio.name, stock.code, stock.short_name, 
+                                           portfolio_stock.units, stock. last FROM Portfolio 
+                                           INNER JOIN Portfolio_Stock ON Portfolio.id == Portfolio_Stock.portfolio_id
+                                           INNER JOIN Stock ON Portfolio_Stock.stock_code == Stock.code 
+                                           WHERE Portfolio.user_id = :val"""
+                                        , {'val': current_user.id})
+            # Convert results to a named tuple
+            Record = namedtuple('Record', result.keys())
+            records = [Record(*r) for r in result.fetchall()]
+            # for r in records:
+            #     print(r.id, r.name, r.code, r.short_name, r.units, r.last)
+            #     print(r)
+            portfolio = records
+            portfolio_value = sum([(i.units * i.last) for i in portfolio])  # calc total value of the portfolio
+            portfolio_name = portfolio[0].name
+            print(portfolio_name)
+            return render_template("home/" + template, segment=segment, portfolio=portfolio, portfolio_value=portfolio_value)
 
         # Serve the file (if exists) from app/templates/home/FILE.html
         return render_template("home/" + template, segment=segment)
